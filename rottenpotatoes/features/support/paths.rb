09f1eece365f1223ef56b2281db6c4563a836bdb
Charles Rothrock <cwrothrock_17@tamu.edu>
2020-03-11 00:25:20 +0000

    Part 1 and Part2/Cucumber done

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -21,6 +21,17 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /^the edit\s?page for "(.*)"$/
+      movie = Movie.where(title: $1).take
+      edit_movie_path(movie.id)
+      
+    when /^the details\s?page for "(.*)"$/
+      movie = Movie.where(title: $1).take
+      movie_path(movie.id)
+      
+    when /^the Similar Movies\s?page for "(.*)"$/
+      search_similar_movies_path($1)
+
     else
       begin
         page_name =~ /^the (.*) page$/
--- rottenpotatoes/spec/rails_helper.rb
+++ rottenpotatoes/spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -5,6 +8,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/factory_bot'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -33,7 +38,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -49,4 +54,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  
+  config.include FactoryBot::Syntax::Methods
 end
